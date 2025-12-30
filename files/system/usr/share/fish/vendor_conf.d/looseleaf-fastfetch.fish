242a5e42d8f63cee09e0f643a95798852e1f25fb
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2025-12-30 11:52:19 -0300

    Align cosmetic branding with Bazzite defaults

---
+++ files/system/usr/share/fish/vendor_conf.d/looseleaf-fastfetch.fish
@@ -0,0 +1,4 @@
+# Looseleaf fastfetch defaults (system-wide, no user paths).
+alias neofetch='/usr/bin/fastfetch -c /usr/share/looseleaf/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/looseleaf/fastfetch.jsonc'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/looseleaf/fastfetch.jsonc'
